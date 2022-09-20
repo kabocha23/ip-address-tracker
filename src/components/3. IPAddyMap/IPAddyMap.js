8ee995a32f541ef09a6917fb99823236ee5c361e
Jason Kobuchi <jekobuchi@gmail.com>
2022-09-20 12:07:30 -0700

    modified leaflet map

--- src/components/3. IPAddyMap/IPAddyMap.js
+++ src/components/3. IPAddyMap/IPAddyMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 import './IPAddyMap.css';
 
 const IPAddyMap = () => {
@@ -11,13 +12,13 @@ const IPAddyMap = () => {
             <div id="map">
                 <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
                     <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
+                    <Marker position={position}>
+                    <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
+                    </Popup>
                     </Marker>
                 </MapContainer>
             </div>
