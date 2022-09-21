392d3b7790df782f7aaee83e9486407229f0218d
Jason Kobuchi <jekobuchi@gmail.com>
2022-09-20 17:20:08 -0700

    modified leaflet map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
 import IPAddyInput from './components/1. IPAddyInput/IPAddyInput';
 import IPAddyRes from './components/2. IPAddyRes/IPAddyRes';
 import IPAddyMap from './components/3. IPAddyMap/IPAddyMap';
 import './App.css';
 
 function App() {
+
+  const [markerPosition, setMarkerPosition] = useState([51.505, -0.09])
+
+
   return (
     <div className="App">
         <IPAddyInput />
         <IPAddyRes />
-        <IPAddyMap />
+        <IPAddyMap 
+          markerPosition={markerPosition} 
+          setMarkerPosition={setMarkerPosition}
+        />
     </div>
   );
 }
--- src/components/3. IPAddyMap/IPAddyMap.js
+++ src/components/3. IPAddyMap/IPAddyMap.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useMap } from 'react-leaflet/hooks'
 import './IPAddyMap.css';
 
-const IPAddyMap = () => {
-
-    const position = [51.505, -0.09]
+const IPAddyMap = ({ markerPosition, setMarkerPosition }) => {
+    
+    const map = useMap();
+    const changePosition = () => {
+      setMarkerPosition([50, -0.5])
+      map.setView(markerPosition);
+      console.log(markerPosition)
+      return null;
+    }
 
     return(
         <div className='ipAddyMap-container'>
+            <p onClick={changePosition}>click here</p>
             <div id="map">
-                <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <MapContainer center={markerPosition} zoom={13} scrollWheelZoom={true}>
                     <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
+                    <Marker position={markerPosition}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Here you go, <br /> Mr. Stalker.
                     </Popup>
                     </Marker>
                 </MapContainer>
